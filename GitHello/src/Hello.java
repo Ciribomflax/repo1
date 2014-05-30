5b31c94ace0615eb6a02f27a2199938b3113c674
neo <neo@vm>
2014-05-30 10:53:40 +0200

    first

---
+++ GitHello/src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		Hello h = new Hello();
+		h.run();
+	}
+
+	
+	public void run(){
+		System.out.println("Hello World!");
+	}
+}
